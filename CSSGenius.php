2d3681a99ce9c85a1803230b509bd5366d564a05
Anna-github123 <73287064+Anna-github123@users.noreply.github.com>
2021-04-29 22:20:22 +0400

    Add files via upload

---
+++ CSSGenius.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>Message Form</title>
+	</head>
+	
+	<body>
+
+        <?php
+            echo " Thank you for your enquiry. You would be contacted within Three (3) working days. ";
+            echo " Thank you for using CSS Genius! ";
+        ?>
+
+    </body>
+</html>
